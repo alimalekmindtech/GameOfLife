f5e006d1af30c0be8e6daf1feb4c5680eacc3d51
Ali <ali.malek@mindtech.global>
2022-06-15 12:18:39 +0100

    nn implemented, test passed

--- game_of_life.py
+++ game_of_life.py
@@ -2,3 +2,34 @@ import numpy as np
 class GoL:
     def __init__(self):
         self.grid = np.zeros((100, 100), dtype=int)
+    
+    def get_nn_idx_set(self, array_idx):
+        idx_row = array_idx[0]
+        idx_col = array_idx[1]
+        idx_row_next = idx_row + 1
+        if idx_row_next >= 100:
+            idx_row_next = None
+
+        idx_row_prev = idx_row - 1
+        if idx_row_prev <0:
+            idx_row_prev = None
+
+        idx_col_next = idx_col + 1
+        if idx_col_next >= 100:
+            idx_col_next = None
+        idx_col_prev = idx_col - 1
+        if idx_col_prev <0:
+            idx_col_prev = None
+
+        idx_nn_row = [idx_row, idx_row_next, idx_row_prev]
+        idx_nn_col = [idx_col, idx_col_next, idx_col_prev]
+
+        result = set()
+        for i_row in idx_nn_row:
+            for i_col in idx_nn_col:
+                if i_row and i_col:
+                    result.add((i_row, i_col))
+        
+        result.remove((idx_row, idx_col))
+
+        return result
--- test_game_of_life.py
+++ test_game_of_life.py
@@ -21,3 +21,7 @@ class TestGoL:
     def test_GridMembers(self):
         grid_elements =set(np.unique(self.gol.grid))
         assert grid_elements.issubset({0,1})
+
+    @pytest.mark.parametrize("array_idx, nn_idx_set", [((2,2), {(1, 3), (2, 3), (3, 3), (1, 2), (3, 2), (1, 1), (2, 1), (3, 1)})])
+    def test_NN_idx(self, array_idx, nn_idx_set):
+        assert self.gol.get_nn_idx_set(array_idx)==nn_idx_set
