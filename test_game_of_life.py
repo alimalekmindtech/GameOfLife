80f5491a71c918341c3c559b942e36e9947e2e6a
Mina <mina.tabrizi@mindtech.global>
2022-06-15 12:39:16 +0100

    ege case

--- test_game_of_life.py
+++ test_game_of_life.py
@@ -2,13 +2,6 @@ import pytest
 from game_of_life import GoL
 import numpy as np
 
-# def foo(a, b):
-#     return a + 1, b + 1
-
-# @pytest.mark.parametrize("a,b", [(1, 2, 3), (-1, -2), (-1, -4), (4, 2)])
-# def test_foo(a, b):
-#     # check that foo runs correctly and that the result is a tuple. 
-#     assert isinstance(foo(a, b), tuple)
 
 class TestGoL:
     def setup_method(self):
@@ -22,6 +15,10 @@ class TestGoL:
         grid_elements =set(np.unique(self.gol.grid))
         assert grid_elements.issubset({0,1})
 
-    @pytest.mark.parametrize("array_idx, nn_idx_set", [((2,2), {(1, 3), (2, 3), (3, 3), (1, 2), (3, 2), (1, 1), (2, 1), (3, 1)})])
+    @pytest.mark.parametrize("array_idx, nn_idx_set", [
+        ((2,2), {(1, 3), (2, 3), (3, 3), (1, 2), (3, 2), (1, 1), (2, 1), (3, 1)}),
+        ((99, 99), {(98, 98), (98, 99), (99, 98)}),
+        
+    ])
     def test_NN_idx(self, array_idx, nn_idx_set):
         assert self.gol.get_nn_idx_set(array_idx)==nn_idx_set
